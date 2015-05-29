369eaf92bde15dd0aef8f0171999b462ea17e17a
MatiasFloresS <matias.flores@usach.cl>
2015-05-29 17:58:10 -0300

    version final no comentada

--- hotpotato.c
+++ hotpotato.c
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
 	int c,t;
 	int index=0;
-	srand (time(NULL));
+	
 	
 
 
@@ -35,6 +35,7 @@ int main(int argc, char** argv) {
   	int rank,size,token,sigo_en_juego=1,cantidad_jugadores,i;
   	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   	MPI_Comm_size(MPI_COMM_WORLD, &size);
+  	srand (time(NULL) + rank);
 
   	token = rand()%t + 1;
   	cantidad_jugadores = size;  	
